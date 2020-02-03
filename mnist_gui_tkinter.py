26eff008738bc55086d29c53c42a8e27d0d72a1d
Gene <4461705+pandalion98@users.noreply.github.com>
2020-02-03 23:37:07 +0800

    working GUI

--- mnist_gui_tkinter.py
+++ mnist_gui_tkinter.py
@@ -1,22 +1,45 @@
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
+import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers import Dense
 
+# Load neural network
+model = load_model(filepath='mnist-100b-relu-chkpt-83-0.98988.h5')
+
+# Define UI elements
 window = Tk()
-window.title("MNIST Test")
+window.title('MNIST Test')
 
 image_label = Label()
 image_label.grid(row=0, column=0)
 
+label2 = Label(text='Prediction: ')
+label2.grid(row=0, column=1)
+label_predict = Label(text='X')
+label_predict.grid(row=0, column=2)
+label3 = Label(text='Rounded: ')
+label3.grid(row=1, column=1)
+label_rounded = Label(text='X')
+label_rounded.grid(row=1, column=2)
+
 
 def button_click():
-    file = filedialog.askopenfile(mode="r", filetypes=[("PNG Images", "*.png")])
-    img = ImageTk.PhotoImage(Image.open(file.name))
-    image_label.image = img
-    image_label.configure(image=img)
+    file = filedialog.askopenfile(mode='r', filetypes=[('PNG Images', '*.png')])
+    img = Image.open(file.name)
+    photo_img = ImageTk.PhotoImage(img)
+    image_label.image = photo_img
+    image_label.configure(image=photo_img)
+    data = np.array(img.getdata()).reshape((1, 28 * 28))
+    predictions = model.predict(data)
+    label_predict = Label(text=predictions)
+    label_predict.grid(row=0, column=2)
+    label_rounded = Label(text=round(predictions[0][0], ndigits=0))
+    label_rounded.grid(row=1, column=2)
 
 
-button1 = Button(window, text="Load image", command=button_click)
+button1 = Button(window, text='Load image', command=button_click)
 button1.grid(row=1, column=0)
 
 window.mainloop()
